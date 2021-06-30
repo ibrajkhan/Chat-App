31dcaf7f16acbf93f3ba69420ea8559d43b2903b
IbrajKhan <ibraj.grd@gmail.com>
2021-06-30 23:12:15 +0530

    Chat app

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Avenir' }}>
+      <Router>
+        
+          <Switch>
+            
+          </Switch>
+       
+      </Router>
+    </div>
+  )
+}
+
+export default App
