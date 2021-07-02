7356a0448e8eafe108d765641481cb379f4aa862
IbrajKhan <ibraj.grd@gmail.com>
2021-07-02 23:03:20 +0530

    Added Google and Facebook Authentication login using firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
 import "../style.css";
+import {auth} from "../firebase"
+import firebase from "firebase/app";
 
 function Login() {
   return (
     <div id="login__page">
       <div id="login__card">
         <h2>Welcome to chat Engine!</h2>
-        <div className="login__button google">
+        <div className="login__button google"
+        onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
           <GoogleOutlined /> Sign In with Google
         </div>
         <br />
         <br />
-        <div className="login__button facebook">
+        <div className="login__button facebook"
+        onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}>
           <FacebookOutlined /> Sign In with Facebook
         </div>
       </div>
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+import 'firebase/auth';
+
+export const auth = firebase.initializeApp({
+    apiKey: "AIzaSyDdGqEV5HceDv6q1wTOIVY1kKrk_vOEuK8",
+    authDomain: "chat-app-94a58.firebaseapp.com",
+    projectId: "chat-app-94a58",
+    storageBucket: "chat-app-94a58.appspot.com",
+    messagingSenderId: "1098877967713",
+    appId: "1:1098877967713:web:6e88057681665b3de5b920"
+  }).auth();
