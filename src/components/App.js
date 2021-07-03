f0e604377e8ade3c39fb7378ebe8e96cd89e2a00
IbrajKhan <ibraj.grd@gmail.com>
2021-07-03 22:53:13 +0530

    Added react Context for handling data for user Login

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthProvider } from "../contexts/AuthContext";
+import Chats from "./Chats";
 
 function App() {
   return (
     <div style={{ fontFamily: "Avenir" }}>
       <Router>
-        <Switch>
-          <Route path="/" component={Login} />
-        </Switch>
+        <AuthProvider>
+          <Switch>
+            <Route path="/" component={Login} />
+            <Route Path="/chats" component={Chats} />
+          </Switch>
+        </AuthProvider>
       </Router>
     </div>
   );
