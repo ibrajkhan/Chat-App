6692c5759497353bf874390dcb6e098e5617c6b1
IbrajKhan <ibraj.grd@gmail.com>
2021-07-04 23:11:06 +0530

    Added chat engine projectId and also done some styling to chats page

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import { ChatEngine } from "react-chat-engine";
-import auth from "../firebase";
+import { auth } from "../firebase";
+import "../style.css";
 
 function Chats() {
+  const history = useHistory();
+  const handelLogOut = async () => {
+    await auth.signOut();
+    history.push("/");
+  };
   return (
-    <div className="chat__page">
+    <div className="chats__page">
       <div className="nav__bar">
         <div className="logo__tab">Chat Engine</div>
-        <div className="logout__tab">Logout</div>
+        <div onClick={handelLogOut} className="logout__tab">
+          Logout
+        </div>
       </div>
-      <ChatEngine />
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectId="316146e5-2748-4e7c-a5b4-cf4c02930fb2"
+        userName="."
+        userSecret="."
+      />
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,13 @@
 import firebase from "firebase/app";
-import 'firebase/auth';
+import "firebase/auth";
 
-export const auth = firebase.initializeApp({
+export const auth = firebase
+  .initializeApp({
     apiKey: "AIzaSyDdGqEV5HceDv6q1wTOIVY1kKrk_vOEuK8",
     authDomain: "chat-app-94a58.firebaseapp.com",
     projectId: "chat-app-94a58",
     storageBucket: "chat-app-94a58.appspot.com",
     messagingSenderId: "1098877967713",
-    appId: "1:1098877967713:web:6e88057681665b3de5b920"
-  }).auth();
+    appId: "1:1098877967713:web:6e88057681665b3de5b920",
+  })
+  .auth();
